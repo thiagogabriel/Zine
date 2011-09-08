b6d1936dc69d1549a778b4fe1c8b3c3d56990d85
Marco Antônio Pompei <marcopompei@me.com>
2011-09-07 23:12:26 -0300

    Working in the models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  has_many :blocks
+  
+  validates_length_of :title, :within => 3..50
 end
--- app/models/block.rb
+++ app/models/block.rb
@@ -1,2 +1,5 @@
 class Block < ActiveRecord::Base
+  belongs_to :article
+  
+  validates_presence_of :article_id, :order, :data, :type
 end
