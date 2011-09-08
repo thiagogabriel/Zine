cb45620a2c8d525c7ec47843912a341abd77d0d7
Marco Antônio Pompei <marcopompei@me.com>
2011-09-07 22:19:50 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zine::Application.config.secret_token = '19d0f970fc62d7a4fafbfa3c89be33117cf45ec0064340ef12c04fa370c0f13bb03d8c81b77966783d846573a5a2999a19cd2694663b603c82692067690c1d6b'
