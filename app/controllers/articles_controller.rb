110de00d2e611a9ae860cc7a68d8f35041c4ec67
Marco Antônio Pompei <marcopompei@me.com>
2011-09-08 20:12:33 -0300

    simpler

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,14 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @articles }
-    end
   end
 
   def show
     @article = Article.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @article }
-    end
   end
 
   def new
     @article = Article.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @article }
-    end
   end
 
   def edit
@@ -33,28 +18,20 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params[:article])
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render json: @article, status: :created, location: @article }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.' }
+    else
+      render action: "new"
     end
   end
 
   def update
     @article = Article.find(params[:id])
 
-    respond_to do |format|
-      if @article.update_attributes(params[:article])
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update_attributes(params[:article])
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
@@ -62,9 +39,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    respond_to do |format|
-      format.html { redirect_to articles_url }
-      format.json { head :ok }
-    end
+    redirect_to articles_url
   end
 end
--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -3,30 +3,15 @@ class BlocksController < ApplicationController
   before_filter :set_params, :only => [:create, :update]
   
   def index
-    @blocks = Block.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @blocks }
-    end
+    @blocks = @article.blocks
   end
 
   def show
     @block = Block.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @block }
-    end
   end
 
   def new
     @block = Block.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @block }
-    end
   end
 
   def edit
@@ -36,43 +21,31 @@ class BlocksController < ApplicationController
   def create
     @block = Block.new(params[:block])
     @block.article = @article
-    @block.block_type = "Text"
-    respond_to do |format|
-      if @block.save
-        format.html { redirect_to @block, notice: 'Block was successfully created.' }
-        format.json { render json: @block, status: :created, location: @block }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @block.errors, status: :unprocessable_entity }
-      end
+    @block.type = "Text"
+    if @block.save
+      redirect_to @block, notice: 'Block was successfully created.' }
+      render action: "new"
     end
   end
 
   def update
     @block = Block.find(params[:id])
 
-    respond_to do |format|
-      if @block.update_attributes(params[:block])
-        format.html { redirect_to @block, notice: 'Block was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @block.errors, status: :unprocessable_entity }
-      end
+    if @block.update_attributes(params[:block])
+      redirect_to @block, notice: 'Block was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
   def destroy
     @block = Block.find(params[:id])
     @block.destroy
-
-    respond_to do |format|
-      format.html { redirect_to blocks_url }
-      format.json { head :ok }
-    end
+    
+    redirect_to blocks_url
   end
   
-  private
+  private #-------------------------------------------
   
   def get_article
     @article = Article.find(params[:article_id])
