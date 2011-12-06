1790f5c3d89248019d67e72ecb40a41153e17da2
Marco Antônio Pompei <marco.fowl@gmail.com>
2011-12-06 20:10:22 -0200

    Imagens usando S3

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,12 @@
 class Image < ActiveRecord::Base
   belongs_to :block
 
-  has_attached_file :image, :styles => {:gallery => "940x320#", :medium => "600x450>", :thumb => "100x100>" }
+  has_attached_file :image,
+    :styles => {:gallery => "940x320#", :medium => "600x450>", :thumb => "100x100>" },
+    :storage => :s3,
+    :bucket => "marcopompei-zine",
+    :s3_credentials => {
+          :access_key_id => ENV['S3_KEY'],
+          :secret_access_key => ENV['S3_SECRET']
+        }
 end
