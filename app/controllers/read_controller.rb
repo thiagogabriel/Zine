0f8dff5799d89ffd2dea4a4bf289a2abbf5ca753
Marco Antônio Pompei <marco.fowl@gmail.com>
2012-05-01 17:13:13 -0300

    Botão para ir para o próximo artigo

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -1,11 +1,13 @@
 class ReadController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.where("publish_at <= ?", Time.now).order(" publish_at DESC").limit(30)
+    @next = @articles[0]
   end
 
   def article
     @article = Article.find(params[:id])
     @blocks = @article.blocks.sort_by { |block| block.order }
+    @next = Article.where("publish_at <= ? AND id <> ?", @article.publish_at, @article.id).order("publish_at DESC").limit(1)[0]
   end
 
 end
